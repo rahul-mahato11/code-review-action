30e81701c41bbfdf6bd43beec7f73c544fee51f1
rahul-mahato <rahul.122293@gmail.com>
2023-03-24 00:10:03 +0530

    log pr

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ try {
   const openaiMaxTokens = core.getInput('openai_max_tokens')
   const mode = core.getInput('mode')
 
+  const octokit = github.getOctokit(githubToken)
 
+  const { data: pullRequest } = await octokit.rest.pulls.get({
+      owner: 'octokit',
+      repo: 'rest.js',
+      pull_number: 123,
+      mediaType: {
+        format: 'diff'
+      }
+  });
+
+  
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(pullRequest);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
