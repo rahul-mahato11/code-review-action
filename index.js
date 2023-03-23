f62a1a9fbe32f8719bd22586bbb1e7704c4e1a8c
rahul-mahato <rahul.122293@gmail.com>
2023-03-24 00:01:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const openaiApiKey = PROCESS.ENV.OPENAI_API_KEY
+  const githubToken = core.getInput('github_token')
+  const githubPrId = core.getInput('github_pr_id')
+  const openaiEngine = core.getInput('openai_engine')
+  const openaiTemperature = core.getInput('openai_temperature')
+  const openaiMaxTokens = core.getInput('openai_max_tokens')
+  const mode = core.getInput('mode')
+
+
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
